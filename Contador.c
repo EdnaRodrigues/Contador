bec2f442ad127761c61f4ee9d5fede6937601757
Edna Rodrigues <ednards2002@gmail.com>
2025-02-01 19:41:21 -0300

    Inclusão do arquivo wokwi (permite a simulação) e correções no código.

--- Contador.c
+++ Contador.c
@@ -4,16 +4,17 @@
 #include "hardware/clocks.h"
 #include "pico/bootrom.h"
 
-//arquivo .pio
+//Arquivo .pio
 #include "Contador.pio.h"
 
-//número de LEDs
+//Número de LEDs
 #define NUM_PIXELS 25
 
-//pino de saída
+//Pino de saída
 #define OUT_PIN 7
+#define pisca_led 13
 
-//botões de interupção
+//Botões de interupção
 #define button_A 5
 #define button_B 6
 #define button_Joy 22
@@ -22,7 +23,7 @@
 static void gpio_irq_handler(uint gpio, uint32_t events) {
     printf("Interrupção ocorreu no pino %d, no evento %d\n", gpio, events);
     printf("HABILITANDO O MODO GRAVAÇÃO");
-	reset_usb_boot(0,0); //habilita o modo de gravação do microcontrolador
+	reset_usb_boot(0,0); //Habilita o modo de gravação do microcontrolador
 }
 
 void inicializa() {
@@ -30,33 +31,37 @@ void inicializa() {
     bool ok;
     double r = 0.0, b = 0.0 , g = 0.0;
 
-    //coloca a frequência de clock para 128 MHz, facilitando a divisão pelo clock
+    //Coloca a frequência de clock para 128 MHz, facilitando a divisão pelo clock
     ok = set_sys_clock_khz(128000, false);
 
     // Inicializa todos os códigos stdio padrão que estão ligados ao binário.
     stdio_init_all();
 
-    //configurações da PIO
+    //Configurações da PIO
     uint offset = pio_add_program(pio, &Contador_program);
     uint sm = pio_claim_unused_sm(pio, true);
     Contador_program_init(pio, sm, offset, OUT_PIN);
-    
-    //inicializar o botão de interrupção para incrementar
+
+    //Inicializar o LED que irá piscar continuamente
+    gpio_init(pisca_led);
+    gpio_set_dir(pisca_led, GPIO_OUT);
+
+    //Inicializar o botão de interrupção para incrementar
     gpio_init(button_A);
     gpio_set_dir(button_A, GPIO_IN);
     gpio_pull_up(button_A);
 
-    //inicializar o botão de interrupção para decrementar
+    //Inicializar o botão de interrupção para decrementar
     gpio_init(button_B);
     gpio_set_dir(button_B, GPIO_IN);
     gpio_pull_up(button_B);
 
-    //inicializar o botão de interrupção para reboot
+    //Inicializar o botão de interrupção para reboot
     gpio_init(button_Joy);
     gpio_set_dir(button_Joy, GPIO_IN);
     gpio_pull_up(button_Joy);
 
-    //interrupções da gpio habilitadas
+    //Interrupções da gpio habilitadas
     gpio_set_irq_enabled_with_callback(button_A, GPIO_IRQ_EDGE_FALL, true, & gpio_irq_handler);
     gpio_set_irq_enabled_with_callback(button_B, GPIO_IRQ_EDGE_FALL, true, & gpio_irq_handler);
     gpio_set_irq_enabled_with_callback(button_Joy, GPIO_IRQ_EDGE_FALL, true, & gpio_irq_handler);
@@ -72,10 +77,12 @@ uint32_t matrix_rgb(double b, double r, double g) {
 
 int main() {
     
-    inicializa();
+    inicializa(); //
 
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        gpio_put(pisca_led, true);
+        sleep_ms(100);
+        gpio_put(pisca_led, false);
+        sleep_ms(100);
     }
 }
